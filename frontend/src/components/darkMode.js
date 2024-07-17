27fb3d6a231fa1bfec09b1449f0c717e4e91fafa
ListowelAdolwin <listoweladolwin@gmail.com>
2024-07-17 08:11:14 +0000

    Updated theme toggling

--- frontend/src/components/darkMode.js
+++ frontend/src/components/darkMode.js
@@ -1,8 +1,10 @@
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
   document.documentElement.classList.add('dark')
+  localStorage.setItem('theme', 'dark')
 } else {
   document.documentElement.classList.remove('dark')
+  localStorage.setItem('theme', 'light')
 }
 
 // Whenever the user explicitly chooses light mode
